07dc8a0efb3a4b5b64b1eb88bea73befc89f15e8
duckeydev <duckeydev@gmail.com>
2025-11-26 20:31:58 +0630

    PATCH: MongoDB with Special characters not being URI encoded
    Signed-off-by: duckeydev <duckeydev@gmail.com>

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,19 +1,28 @@
-
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const logger = require('../logger').default;
+
 dotenv.config();
+
+const sanitize = (uri) => {
+    try {
+        const u = new URL(uri);
+        if (u.password) u.password = encodeURIComponent(u.password);
+        return u.toString();
+    } catch {
+        return uri;
+    }
+};
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-    
+        const safe = sanitize(process.env.MONGO_URI);
+        const conn = await mongoose.connect(safe);
         logger.success(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         logger.info(`Error: ${error.message}`);
         process.exit(1);
     }
-}
-
+};
 
 module.exports = connectDB;
